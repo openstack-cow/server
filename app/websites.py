04e7c62115a8051f711729147508e69ab5edcd6b
darklord1611 <christopher061204@gmail.com>
2024-12-08 09:18:43 +0000

    fix: add temporary fake website data

---
+++ app/websites.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request, jsonify, render_template
+import json
+
+
+websites = Blueprint('websites', __name__)
+
+@websites.route("/<string:id>", methods=["GET"])
+def get_website_info(id):
+    print(id)
+    with open("app/fake_data/website_info.json", "r") as json_file:
+            data = json.load(json_file)
+    return jsonify(data), 200
