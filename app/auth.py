967134349af082b6f3f0636f879b5fcfb99d8762
Vu Tung Lam <vutunglaminfo@gmail.com>
2024-12-16 14:02:17 +0000

    refactor

--- app/auth.py
+++ app/auth.py
@@ -1,32 +1,12 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, logout_user
 from .models import User
 from . import db
 import jwt
 import datetime
-from functools import wraps
-
+from .utils.token_required import token_required
 
 auth = Blueprint('auth', __name__)
-
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.headers.get('Authorization')  # Lấy token từ header
-        if not token:
-            return jsonify({"error": "Token is missing!"}), 401
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
-            current_user = User.query.get(data['user_id'])
-            if not current_user:
-                return jsonify({"error": "Invalid token!"}), 401
-        except jwt.ExpiredSignatureError:
-            return jsonify({"error": "Token has expired!"}), 401
-        except jwt.InvalidTokenError:
-            return jsonify({"error": "Invalid token!"}), 401
-        return f(current_user, *args, **kwargs)
-    return decorated
 
 @auth.route('/signup', methods=['POST'])
 def signup():
@@ -58,12 +38,11 @@ def signup():
         return jsonify({"error": "Password must be at least 7 characters."}), 400
 
     # Create new user
-    new_user = User(name=name, email=email, password=generate_password_hash(password, method='pbkdf2:sha256'))
+    new_user = User(name=name, email=email, password=generate_password_hash(password, method='pbkdf2:sha256')) # type: ignore
     db.session.add(new_user)
     db.session.commit()
 
     return jsonify({"message": f"Account created successfully! Welcome, {name}"}), 200
-
 
 @auth.route('/login', methods=['POST'])
 def login():
---
+++ app/utils/token_required.py
@@ -0,0 +1,24 @@
+from flask import request, jsonify
+import app.models
+import jwt
+
+import app.env
+from functools import wraps
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')  # Lấy token từ header
+        if not token:
+            return jsonify({"error": "Token is missing!"}), 401
+        try:
+            data = jwt.decode(token, app.env.SECRET_KEY, algorithms=["HS256"])
+            current_user = app.models.User.query.get(data['user_id'])
+            if not current_user:
+                return jsonify({"error": "Invalid token!"}), 401
+        except jwt.ExpiredSignatureError:
+            return jsonify({"error": "Token has expired!"}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({"error": "Invalid token!"}), 401
+        return f(current_user, *args, **kwargs)
+    return decorated
