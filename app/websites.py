8317933e467ad42ec6b9667347f74c7ba6de796a
ho1ow <nguyenminhchaucnt@gmail.com>
2024-12-24 10:44:25 +0700

    update websites api

--- app/websites.py
+++ app/websites.py
@@ -1,131 +1,173 @@
 from flask import Blueprint, request, jsonify, render_template
-from .models import Website, CinderVolume, NovaVM, User, Plan
+import paramiko
+from .models import Website, NovaVM, User, Plan
 import json
 from .utils.token_required import token_required
 from .utils.openstack_api import get_openstack_connection
 from . import db
+from .env import NOVA_VM_PRIVATE_KEY_PATH
 websites = Blueprint('websites', __name__)
-conn=get_openstack_connection()
+conn = get_openstack_connection()
+
+
 @websites.route("/test", methods=["GET"])
 def test():
-        users = User.query.all()  # Fetch all users
-        # Serialize the query results into a list of dictionaries
-        users_data = [{"id": user.id, "email": user.email, "name": user.name} for user in users]
-        return jsonify({"data": users_data})  # Return serialized data
+    users = User.query.all()  # Fetch all users
+    # Serialize the query results into a list of dictionaries
+    users_data = [{"id": user.id, "email": user.email,
+                   "name": user.name} for user in users]
+    return jsonify({"data": users_data})  # Return serialized data
+
+
 @websites.route("/<int:id>", methods=["GET"])
 def get_website_info(id):
     print(id)
     with open("app/fake_data/website_info.json", "r") as json_file:
-            data = json.load(json_file)
+        data = json.load(json_file)
     return jsonify(data), 200
-@websites.route("/",methods=["GET"])
+
+
+@websites.route("/", methods=["GET"])
 @token_required
-def get_all_website_info(): 
-        all_websites = Website.query.all()
-        websites_info = []
-        for website in all_websites:
-            websites_info.append(get_website_detail(website.id))
-        return jsonify({"data": websites_info}), 200
+def get_all_website_info():
+    all_websites = Website.query.all()
+    websites_info = []
+    for website in all_websites:
+        websites_info.append(get_website_detail(website.id))
+    return jsonify({"data": websites_info}), 200
+
+
 @websites.route("/<int:id>", methods=["GET"])
 @token_required
-def get_website_info_detail( id):
-        website_info=get_website_detail(id)
-        return jsonify({"data":website_info}), 200       
-@websites.route("/<int:id>",methods=["DELETE"])
+def get_website_info_detail(id):
+    website_info = get_website_detail(id)
+    return jsonify({"data": website_info}), 200
+
+
+@websites.route("/<int:id>", methods=["DELETE"])
 @token_required
 def delete_website(id):
-        try:
-                website = Website.query.filter_by(id=id).first()
+    try:
+        website = Website.query.filter_by(id=id).first()
+        docker_compose_path= f"/home/ubuntu/web_{id}/"
+        action =f"cd {docker_compose_path} && docker-compose down -v"
+        nova_vm_shell(action, id)
+        if not website:
+            return jsonify({"error": "Website not found"}), 404
 
-                if not website:
-                        return jsonify({"error": "Website not found"}), 404
+        db.session.delete(website)
+        db.session.commit()
 
-                db.session.delete(website)
-                db.session.commit()
+        return jsonify({"message": f"Website with ID {id} has been deleted successfully"}), 200
 
-                return jsonify({"message": f"Website with ID {id} has been deleted successfully"}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
 
-        except Exception as e:
-                db.session.rollback()
-                return jsonify({"error": str(e)}), 500
 
 @websites.route("/<int:id>/stauts", methods=["POST"])
 @token_required
 def update_website_status(id):
-        try:
-                data = request.get_json()
-                action = data.get("action")
+    try:
+        data = request.get_json()
+        action = data.get("action")
+        action_shell= f"docker {action} web_{id}"
+        valid_actions = {"start", "stop", "restart"}
+        if action not in valid_actions:
+            return jsonify({"error": f"Invalid action. Valid actions are: {', '.join(valid_actions)}"}), 400
 
-                valid_actions = {"start", "stop", "restart", "reset"}
-                if action not in valid_actions:
-                        return jsonify({"error": f"Invalid action. Valid actions are: {', '.join(valid_actions)}"}), 400
+        website = Website.query.filter_by(id=id).first()
+        if not website:
+            return jsonify({"error": "Website not found"}), 404
 
-                website = Website.query.filter_by(id=id).first()
-                if not website:
-                        return jsonify({"error": "Website not found"}), 404
+        if action == "start":
+            website.status = "running"
+        elif action == "stop":
+            website.status = "stopped"
+        elif action == "restart":
+            website.status = "restarting"
 
-                if action == "start":
-                        website.status = "running"
-                elif action == "stop":
-                        website.status = "stopped"
-                elif action == "restart":
-                        website.status = "restarting"
-                elif action == "reset":
-                        website.status = "resetting"
-
-                db.session.commit()
-
-                return jsonify({
-                        "message": f"Action '{action}' performed successfully.",
-                        "website_id": website.id,
-                        "new_status": website.status
-                }), 200
-
-        except Exception as e:
-                db.session.rollback()  
-                return jsonify({"error": str(e)}), 500
+        nova_vm_shell(action_shell, id)
         
+        db.session.commit()
+
+        return jsonify({
+            "message": f"Action '{action}' performed successfully.",
+            "website_id": website.id,
+            "new_status": website.status
+        }), 200
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
 
 def get_website_detail(id):
-        website_data=Website.query.filter_by(id=id)
-        if not website_data:
-                return jsonify({"error": "Website not found"}), 404
-        plan_data = Plan.query.filter_by(id=website_data.plan_id).first()
-        if plan_data.has_mysql:
-                mysql_details = {
-                        "mysql_host": "localhost",  
-                        "mysql_port": 3306,         
-                        "mysql_user": "root",       
-                        "mysql_password": "password",
-                        "mysql_database": "abc"
-                }
-        if plan_data.has_redis:
-                redis_url = f"redis://localhost:6379/abc"  
-
-        plan = {
-                "id": plan_data.id,
-                "storage_in_gb": plan_data.storage_in_gb, 
-                "name": plan_data.name,
-                "price": plan_data.price
-        } if plan_data else None
-        website_details = {
-                "id": website_data.id,
-                "name": website_data.name,
-                "plan_id": website_data.plan_id,
-                "status": website_data.status,
-                "address": f"http://{website_data.public_port}",
-                "plan": plan,
-                "redis_url": redis_url,
-                "mysql": mysql_details
+    website_data = Website.query.filter_by(id=id)
+    if not website_data:
+        return jsonify({"error": "Website not found"}), 404
+    plan_data = Plan.query.filter_by(id=website_data.plan_id).first()
+    if plan_data.has_mysql:
+        mysql_details = {
+            "mysql_host": "localhost",
+            "mysql_port": 3306,
+            "mysql_user": "root",
+            "mysql_password": "password",
+            "mysql_database": "abc"
         }
-        return jsonify(website_details), 200
+    if plan_data.has_redis:
+        redis_url = f"redis://localhost:6379/abc"
 
-def get_nova_detail(id):
-        novaVm=conn.compute.get_server(id)
-        if not novaVm:
-                return jsonify({"error": "Nova VM not found"}), 404
-        vm_info= {
-                "vm_id":novaVm.id,
-        }
-def get_cinder_detail(id):
-        return
+    plan = {
+        "id": plan_data.id,
+        "storage_in_gb": plan_data.storage_in_gb,
+        "name": plan_data.name,
+        "price": plan_data.price
+    } if plan_data else None
+    website_details = {
+        "id": website_data.id,
+        "name": website_data.name,
+        "plan_id": website_data.plan_id,
+        "status": website_data.status,
+        "address": f"http://{website_data.public_port}",
+        "plan": plan,
+        "redis_url": redis_url,
+        "mysql": mysql_details
+    }
+    return jsonify(website_details), 200
+
+
+def nova_vm_shell(action, website_id):
+    nova_floating_ip = Website.join(NovaVM, Website.nova_vm_id == NovaVM.id).filter(
+        Website.id == website_id).first().floating_ip
+    nova_name = "ubuntu"
+    port = 22
+    private_key_path = NOVA_VM_PRIVATE_KEY_PATH  
+
+    try:
+        private_key = paramiko.Ed25519Key.from_private_key_file(private_key_path)
+
+        # Create an SSH client
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(
+            paramiko.AutoAddPolicy())  
+
+        ssh_client.connect(nova_floating_ip, port, username=nova_name, pkey=private_key)
+        print("Connected to the server")
+
+        stdin, stdout, stderr = ssh_client.exec_command(action)
+
+        output = stdout.read().decode().strip()
+        error = stderr.read().decode().strip()
+
+        if output:
+            print(f"Command Output:\n{output}")
+        if error:
+            print(f"Command Error:\n{error}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return jsonify({"error": str(e)}), 500
+    finally:
+        ssh_client.close()
+        print("Connection closed")
